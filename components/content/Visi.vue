f1aece379518a64cb1d667be00fae015bdce75c9
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-12-24 00:28:22 +0700

    Refactor Visi.vue component to use named slots for each tab content

--- components/content/Visi.vue
+++ components/content/Visi.vue
@@ -1,18 +1,52 @@
+<!-- eslint-disable vue/no-unused-vars -->
+<!-- eslint-disable vue/no-unused-vars -->
 <script setup>
-const items = [{
-  label: 'Visi',
-  content: 'This is the content shown for Tab1',
-}, {
-  label: 'Tujuan',
-  content: 'And, this is the content for Tab2',
-}, {
-  label: 'Misi',
-  content: 'Finally, this is the content for Tab3',
-}]
+const items = [
+  {
+    label: 'Visi',
+    slot: 'visi',
+  },
+  {
+    label: 'Tujuan',
+    slot: 'tujuan',
+  },
+  {
+    label: 'Misi',
+    slot: 'misi',
+  },
+]
 </script>
 
 <template>
   <UContainer class="py-16 md:py-24">
-    <UTabs :items="items" :default-index="1" />
+    <UTabs :items="items" :default-index="1">
+      <template #misi>
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          class="px-6 bg-slate-100 shadow dark:bg-slate-900 py-2 rounded-md"
+        >
+          <div>ini misi</div>
+        </div>
+      </template>
+      <template #tujuan>
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          class="px-6 bg-slate-100 shadow dark:bg-slate-900 py-2 rounded-md"
+        >
+          <div>ini tujuan</div>
+        </div>
+      </template>
+      <template #visi>
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          class="px-6 bg-slate-100 shadow dark:bg-slate-900 py-2 rounded-md"
+        >
+          <div>ini visi</div>
+        </div>
+      </template>
+    </UTabs>
   </UContainer>
 </template>
