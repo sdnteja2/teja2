f1dda0314f13e110ebbee23f5373ea88fdfadaa7
narr07 <dinarpermadi07@gmail.com>
2024-12-16 08:19:34 +0700

    initialize Nuxt.js application with TypeScript, configuration files, and basic UI setup

---
+++ app.config.ts
@@ -0,0 +1,6 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'merah',
+    gray: 'slate',
+  }
+})
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@nuxt/content",
+    "@vueuse/nuxt"
+  ],
+  fonts: {
+    families: [
+      {
+        name: 'Montserrat',
+        provider: 'google',
+        weights: [200, 300, 400, 500, 600, 700, 800, 900], // Heading font
+        styles: ['normal', 'italic'],
+      },
+      {
+        name: 'Plus Jakarta Sans',
+        provider: 'google',
+        weights: [200, 300, 400, 500, 600, 700, 800, 900], // Body font
+        styles: ['normal', 'italic'],
+      },
+    ],
+  },
+  compatibilityDate: "2024-12-16"
+})
