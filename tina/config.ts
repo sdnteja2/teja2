2cb89647c5dde8437435072948f67ae8efca7896
narr07 <dinarpermadi07@gmail.com>
2024-12-16 11:50:10 +0700

    add branch configuration to TinaCMS setup

--- tina/config.ts
+++ tina/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tinacms";
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || "master";
 export default defineConfig({
+  branch,
   clientId: process.env.NUXT_TINA_CLIENT_ID || "", // Get this from tina.io
   token: process.env.NUXT_TINA_TOKEN || "", // Get this from tina.io
 
