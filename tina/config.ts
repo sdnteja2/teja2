2e28ce5a2c8eb53f074f59de72fcfefe7e1b7c0b
narr07 <dinarpermadi07@gmail.com>
2024-12-16 11:39:41 +0700

    add initial configuration and setup for TinaCMS, Netlify functions, and content structure

---
+++ tina/config.ts
@@ -0,0 +1,87 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    loadCustomStore: async () => {
+      const pack = await import("next-tinacms-cloudinary");
+      return pack.TinaCloudCloudinaryMediaStore;
+    },
+  },  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "hero", // Menambahkan collection hero untuk pengaturan hero section
+        label: "Hero Section",
+        path: "content",
+        format: "json",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "subtitle",
+            label: "Subtitle",
+          },
+          {
+            type: "image",
+            name: "backgroundImage",
+            label: "Background Image",
+          },
+          {
+            type: "string",
+            name: "ctaButtonText",
+            label: "CTA Button Text",
+          },
+          {
+            type: "string",
+            name: "ctaButtonLink",
+            label: "CTA Button Link",
+          },
+        ],
+      },
+      
+    ],
+  },
+});
